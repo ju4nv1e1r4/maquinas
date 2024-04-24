8fe46cc48eb747e52da6ce2c94ff9ded5863d35a
Juan Vieira <161749246+ju4nv1e1r4@users.noreply.github.com>
2024-04-24 11:41:38 -0300

    Update maquinas.sql

--- maquinas.sql
+++ maquinas.sql
@@ -15,6 +15,7 @@ WHERE nome IN ('Maquina 01', 'Maquina 02', 'Maquina 03')
 GROUP BY nome
 
 -- Montante e média de cada maquina
+
 SELECT nome, SUM(quantidade) AS TOTAL, ROUND(AVG(quantidade),2) AS media
 FROM maquinas
 WHERE nome IN ('Maquina 01', 'Maquina 02', 'Maquina 03')
@@ -35,14 +36,13 @@ SELECT nome, SUM(quantidade) AS TOTAL, ROUND(AVG(quantidade),2) AS media
 FROM maquinas
 WHERE nome IN ('Maquina 01', 'Maquina 02', 'Maquina 03')
 GROUP BY nome
-ORDER BY "media" DESC;
+ORDER BY media DESC;
 
 -- Calculando a moda
 
-SELECT quantidade, COUNT(*) AS moda
+SELECT nome, MODE() WITHIN GROUP(ORDER BY quantidade) AS MODA
 FROM maquinas
-GROUP BY quantidade
-ORDER BY 2 DESC;
+GROUP BY nome;
 
 -- Calculando a amplitude
 
@@ -66,7 +66,7 @@ SELECT nome,
 	FROM maquinas
 	GROUP BY nome
 	ORDER BY 4 DESC;
-
+	
 -- Calculando a mediana
 
 CREATE OR REPLACE FUNCTION _final_median(NUMERIC[])
@@ -82,7 +82,7 @@ $$
    ) sub;
 $$
 LANGUAGE 'sql' IMMUTABLE;
-								 
+										 
 CREATE AGGREGATE median(NUMERIC) (
   SFUNC=array_append,
   STYPE=NUMERIC[],
@@ -92,5 +92,35 @@ CREATE AGGREGATE median(NUMERIC) (
 
 SELECT MEDIAN(quantidade) AS MEDIANA
 FROM maquinas;
+
+SELECT nome, ROUND(MEDIAN(quantidade),2) AS MEDIANA
+FROM maquinas
+WHERE nome IN ('Maquina 01', 'Maquina 02', 'Maquina 03')
+GROUP BY nome;
+
+-- todas juntas numa query só.
+
+SELECT nome,
+	COUNT(quantidade) AS QTD,
+	SUM(quantidade) AS TOTAL,
+	ROUND(AVG(quantidade),2) AS MEDIA,
+	MAX(quantidade) - MIN(quantidade) AS AMPLITUDE_TOTAL,
+	ROUND(VAR_POP(quantidade),2) AS VARIANCIA,
+	ROUND(STDDEV_POP(quantidade),2) AS DESVIO_PADRAO,
+	ROUND(MEDIAN(quantidade),2) AS MEDIANA,
+	ROUND((STDDEV_POP(quantidade) / AVG(quantidade)) * 100,2) AS COEF_VAR,
+	MODE() WITHIN GROUP(ORDER BY quantidade) AS MODA
+	   FROM maquinas
+	   GROUP BY nome
+	   ORDER BY 1;
+
+-- alguns inserts a mais
+
+INSERT INTO MAQUINAS VALUES('Maquina 01',11,15.9);
+INSERT INTO MAQUINAS VALUES('Maquina 02',11,15.4);
+INSERT INTO MAQUINAS VALUES('Maquina 03',11,15.7);
+INSERT INTO MAQUINAS VALUES('Maquina 01',12,30);
+INSERT INTO MAQUINAS VALUES('Maquina 02',12,24);
+INSERT INTO MAQUINAS VALUES('Maquina 03',12,45);
 
 
